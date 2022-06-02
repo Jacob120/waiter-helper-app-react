b430ba999ff4ab769fef93322263cf32ed46b4d8
Jakub Kozik <jakubkozik120@gmail.com>
2022-06-02 21:53:37 +0200

    Add TableForm.js

--- src/components/features/TableForm.js
+++ src/components/features/TableForm.js
@@ -8,7 +8,9 @@ const TableForm = ({ action, actionText, ...props}) => {
 
   const statuses = useSelector(getAllStatuses);
 
-  const [status, setStatus] = useState(props.status || '')
+  const [status, setStatus] = useState(props.status || '');
+  const [people, setPeople] = useState(props.people || '');
+  const [maxPeople, setMaxPeople] = useState(props.maxPeople || '');
   
 
   const { register, handleSubmit: validate, formState: { errors } } = useForm();
@@ -21,18 +23,49 @@ const TableForm = ({ action, actionText, ...props}) => {
     <Row>
       <h1 className="my-5">Table 1</h1>
       <Form onSubmit={validate(handleSubmit)}>
-        <Form.Group>
-          <Form.Label><b>Status: </b></Form.Label>
-          <Form.Control {...register("category", { required: true})}
-              as="select"
-              placeholder="Please select category"
-              value={status ? status : "1"}
-              onChange={e => setStatus(e.target.value)}>
-                <option disabled value="1">Select status...</option>
-               {statuses.map((status, index) => <option key={index} value={status}>{status}</option> )}   
-            </Form.Control> 
+        <Form.Group as={Row} className="mb-3">
+          <Form.Label column sm="1"><b>Status: </b></Form.Label>
+          <Col sm="10">
+            <Form.Control {...register("category", { required: true})}
+                as="select"
+                placeholder="Please select category"
+                value={status ? status : "1"}
+                onChange={e => setStatus(e.target.value)}
+                className="w-25"
+                >
+                  <option disabled value="1">Select status...</option>
+                {statuses.map((status, index) => <option key={index} value={status}>{status}</option> )}   
+              </Form.Control> 
+          </Col>
             {/* {statusError && <small className="d-block form-text text-danger mt-2">Please choose category</small>}       */}
+        </Form.Group>
+        <Form.Group as={Row}>
+          <Form.Label column sm="1"><b>People: </b></Form.Label>
+          <Col sm="1"  >
+            <Form.Control 
+                {...register("people", { required: true})}
+                value={people} 
+                type="text"                
+                onChange={e => setPeople(e.target.value)} 
+               style={{width: '5'}}
 
+              />
+              {errors.title && <small className="d-block form-text text-danger mt-2">This field is required and has to be at least 3 characters long</small>}
+          </Col>
+          <Col sm="1">
+            <p>/</p>
+          </Col>
+          <Col sm="1"  >
+            <Form.Control 
+                {...register("maxPeople", { required: true})}
+                value={maxPeople} 
+                type="text"                
+                onChange={e => setMaxPeople(e.target.value)} 
+               style={{width: '5'}}
+
+              />
+              {errors.title && <small className="d-block form-text text-danger mt-2">This field is required and has to be at least 3 characters long</small>}
+          </Col>
         </Form.Group>
       </Form>
     </Row>
